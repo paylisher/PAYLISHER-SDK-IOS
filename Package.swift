7bc1200a6d7495cb478f1b7988aa8e79730f2d24
RasimBurakKaya <kayarasimburak@gmail.com>
2025-03-25 14:43:41 +0300

    Package.swift file is updated

--- Package.swift
+++ Package.swift
@@ -7,21 +7,23 @@ let package = Package(
         .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6),
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        // Kaynak koddan üretilen kütüphane
         .library(
             name: "Paylisher",
             targets: ["Paylisher"]
         ),
+        // Binary olarak oluşturulmuş .xcframework ürününüz
+        .library(
+            name: "PaylisherFramework",
+            targets: ["PaylisherFramework"]
+        )
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/Quick/Quick.git", from: "6.0.0"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "12.0.0"),
         .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", from: "9.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Paylisher",
             path: "Paylisher",
@@ -47,3 +49,4 @@ let package = Package(
         )
     ]
 )
+
