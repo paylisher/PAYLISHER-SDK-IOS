68932416138bf7747c4917f948550eef16c93b26
RasimBurakKaya <kayarasimburak@gmail.com>
2025-03-25 12:29:57 +0300

    xcframework file is created.

---
+++ PaylisherFrameworkTests/PaylisherFrameworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  PaylisherFrameworkTests.swift
+//  PaylisherFrameworkTests
+//
+//  Created by Rasim Burak Kaya on 25.03.2025.
+//
+
+import Testing
+@testable import PaylisherFramework
+
+struct PaylisherFrameworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
